6694cca542e9e47e7b59e98e2ad59d5ad9c162b1
Bryan Arnold <PureMath86@users.noreply.github.com>
2017-12-27 11:22:50 -0600

    Update settings.py

--- realcost/settings.py
+++ realcost/settings.py
@@ -1,48 +1,16 @@
-"""
-Django settings for realcost project.
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
+import dj_database_url
+from decouple import Csv, config
+from unipath import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'realcost/static')
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'realcost/static_temp'),
-)
-
+PROJECT_DIR = Path(__file__).parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
+SECRET_KEY = config('SECRET_KEY')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# PostgreSQL conf
-POSTGRES_PASSWORD=8274071216e7b2fd378a1bc73a29009cead9d27555f79cc216b40d2935c77360
-POSTGRES_USER=kxpsmdsmqvsovq
-POSTGRES_DB=d2ncg2n0dpkp9l
-DATABASE_URL=postgres://kxpsmdsmqvsovq:8274071216e7b2fd378a1bc73a29009cead9d27555f79cc216b40d2935c77360@ec2-50-19-251-65.compute-1.amazonaws.com:5432/d2ncg2n0dpkp9l
-
-# Main environmental variables
-DEBUG=False
-SECRET_KEY=s3cr3t_key
-ALLOWED_HOSTS=*
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -50,46 +18,17 @@ DATABASES = {
     )
 }
 
-
-ALLOWED_HOSTS = []
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            # insert your TEMPLATE_DIRS here
-            os.path.join(BASE_DIR, 'realcost/templates'),
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-                # list if you haven't customized them:
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'crispy_forms'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -97,19 +36,36 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 )
 
 ROOT_URLCONF = 'realcost.urls'
 
 WSGI_APPLICATION = 'realcost.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            PROJECT_DIR.child('templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': DEBUG
+        },
+    },
+]
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -121,5 +77,34 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = (
+    ('en', 'English'),
+    ('pt-br', 'Portuguese'),
+    ('es', 'Spanish'),
+    ('zh-cn', 'Chinese'),
+)
 
+LOCALE_PATHS = (PROJECT_DIR.child('locale'), )
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = PROJECT_DIR.parent.child('staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child('static'),
+)
+
+MEDIA_ROOT = PROJECT_DIR.parent.child('media')
+MEDIA_URL = '/media/'
+
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/feeds/'
+
+ALLOWED_SIGNUP_DOMAINS = ['*']
+
+FILE_UPLOAD_TEMP_DIR = '/tmp/'
+FILE_UPLOAD_PERMISSIONS = 0o644
+
+TAGGIT_CASE_INSENSITIVE = True
