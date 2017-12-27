dde4a0eec760c13e64b1efd0451355504b018dab
Bryan Arnold <PureMath86@users.noreply.github.com>
2017-12-27 14:28:04 -0600

    Update settings.py

--- realcost/settings.py
+++ realcost/settings.py
@@ -10,6 +10,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
