441e494c4cc90ba9ee14b41be1ad8c7165282c5f
Bryan Arnold <PureMath86@users.noreply.github.com>
2017-12-27 11:13:52 -0600

    Update settings.py

--- realcost/settings.py
+++ realcost/settings.py
@@ -108,16 +108,6 @@ ROOT_URLCONF = 'realcost.urls'
 WSGI_APPLICATION = 'realcost.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
