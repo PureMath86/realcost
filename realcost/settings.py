0922f2144a50416af22648cbae79e7d635f42892
Bryan Arnold <PureMath86@users.noreply.github.com>
2017-12-27 10:29:50 -0600

    Update settings.py

--- realcost/settings.py
+++ realcost/settings.py
@@ -16,14 +16,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'realcost/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/realcost/static')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'realcost/static_temp'),
+    os.path.join(BASE_DIR, '/realcost/static_temp'),
 )
 
 
